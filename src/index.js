cf99a24f7446b088b6ac6dc31e82f7ec0841185a
susan <susan@users.noreply.github.com>
2022-10-22 12:00:29 -0400

    change rating and comment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ let nameTag = document.querySelector(".name");
 let restaurantNameTag = document.querySelector(".restaurant");
 let ratingTag = document.querySelector("input[name=rating]");
 let commentTag = document.querySelector("textarea[name=comment]");
+let ratingForm = document.querySelector("#ramen-rating");
+console.log(ratingForm);
 
 //fetch
 fetch("http://localhost:3000/ramens")
@@ -20,13 +22,45 @@ fetch("http://localhost:3000/ramens")
 
 imagesContainerTag.addEventListener("click", (e) => {
   let id = e.target.dataset.id;
+  insertRestaurant(id);
+});
+
+//functions
+
+const insertRestaurant = (id) => {
   fetch(`http://localhost:3000/ramens/${id}`)
     .then((r) => r.json())
     .then((r) => {
+      ratingForm.dataset.id = id;
       restaurantImageTag.src = r.image;
       nameTag.innerText = r.name;
       restaurantNameTag.innerText = r.restaurant;
       ratingTag.value = r.rating;
       commentTag.textContent = r.comment;
     });
-});
+};
+
+const changeRating = (e) => {
+  e.preventDefault();
+  let newRating = e.target.rating.value;
+  let newComment = e.target.comment.value;
+  let id = ratingForm.dataset.id;
+
+  fetch(`http://localhost:3000/ramens/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      rating: newRating,
+      comment: newComment,
+    }),
+  })
+    .then((r) => r.json())
+    .then((r) => {
+      console.log(r);
+    });
+};
+
+//listeners
+ratingForm.addEventListener("submit", changeRating);
